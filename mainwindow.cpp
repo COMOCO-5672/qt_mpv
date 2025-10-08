ef03bd8c2035f96acf7505eebc528fd8d067129f
培伦 焦 <775324623@qq.com>
2025-10-08 19:55:25 +0800

    先能播放处理文件

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,36 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QDir>
+#include <QTimer>
+#include <QStandardPaths>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    mpvWidget = new MpvWidget(this);
+    setCentralWidget(mpvWidget);
+    
+    setWindowTitle("Qt MPV Player");
+    //resize(800, 600);
+    
+    // 自动播放视频
+    QTimer::singleShot(100, this, [this]() {
+        // 自动查找视频文件
+        QString videoDir = QStandardPaths::writableLocation(QStandardPaths::MoviesLocation);
+        QDir dir(videoDir);
+        QStringList videos = dir.entryList({"*.mp4", "*.mkv", "*.avi", "*.mov", "*.mpg"}, QDir::Files);
+        
+        if (!videos.isEmpty()) {
+            QString file = dir.filePath(videos.first());
+            mpvWidget->loadFile(file);
+            mpvWidget->play();
+            showFullScreen();
+        }
+    });
 }
 
 MainWindow::~MainWindow()
